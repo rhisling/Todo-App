c1e1e68e7b7c9750378fece99d86a1128cb5f2f2
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-06-08 19:27:43 -0700

    initial setup

---
+++ notes.js
@@ -0,0 +1,33 @@
+console.log("starting notes.js");
+
+const fs = require("fs");
+
+var addNote = (title, body) => {
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  notes.push(note);
+
+  fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+};
+
+var getAll = () => {
+  console.log("Getting all notes");
+};
+
+var getNote = title => {
+  console.log("Getting notes", title);
+};
+
+var removeNote = title => {
+  console.log("Removing notes", title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote
+};
